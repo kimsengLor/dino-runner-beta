8e0fade7c93dfd82cfc659d1d7ca286c0245a5b2
Kimseng Lor <134681132+kimsengLor@users.noreply.github.com>
2026-02-04 11:22:50 +0700

    Add files via upload
    combine everything

---
+++ main.gd
@@ -0,0 +1,31 @@
+extends Node
+
+#game variables
+const DINO_START_POS := Vector2i(150, 485)
+const CAM_START_POS := Vector2i(576, 324)
+
+var speed : float
+const START_SPEED : float = 5.0
+const MAX_SPEED : int = 25
+var screen_size : Vector2i
+
+#called wehen the node enters the sence tree for the first time.
+func _ready():
+	screen_size = get_window().size
+	new_game()
+
+func new_game():
+	$Dino.position = DINO_START_POS
+	$Dino.velocity = Vector2i(0, 0)
+	$Camera2D.position = CAM_START_POS
+	$Ground.position = Vector2i(0, 0)
+#called every frame
+func _process(delta):
+	speed = START_SPEED
+	
+	#move dino and camera    
+	$Dino.position.x += speed   
+	$Camera2D.position.x += speed
+	
+	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+		$Ground.position.x += screen_size.x
